6d8a45874733321c3272ac4d90d4f131b09c6c6a
moin <moinur.hossain@yahoo.com>
2024-11-21 18:05:44 +0600

    Reformat Code

--- src/main/java/com/example/licenseapi/controller/LicenseController.java
+++ src/main/java/com/example/licenseapi/controller/LicenseController.java
@@ -4,7 +4,6 @@ import com.example.licenseapi.model.License;
 import com.example.licenseapi.service.IvasIntegrationService;
 import com.example.licenseapi.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
